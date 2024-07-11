c636d51402f3e8b8db2edf843ad68ef3f8665418
Anthony Trépanier <38358928+Rangerz132@users.noreply.github.com>
2024-07-10 20:29:45 -0400

    add quit application button

---
+++ Assets/Scripts/GUI/QuitApplication.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    void Start() { }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
