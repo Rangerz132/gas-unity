8d65b55287f9cefbb68aad949f5be8749af8b028
Anthony Trépanier <38358928+Rangerz132@users.noreply.github.com>
2024-07-16 20:46:03 -0400

    add health pop

--- Assets/Scripts/Pop/Health/HealthPop.cs
+++ Assets/Scripts/Pop/Health/HealthPop.cs
@@ -2,22 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
 
 public class HealthPop : MonoBehaviour, IPoolable
 {
     [SerializeField] private TextMeshPro textMeshPro;
-    [SerializeField] private float movementSpeed;
 
-    void Update()
-    {
-        float targetY = transform.position.y + (Time.deltaTime * movementSpeed);
-        transform.position = new Vector3(transform.position.x, targetY, transform.position.z);
-    }
+    [Header("Tween Movement")]
+    [SerializeField] private Vector3 movementValue;
+    [SerializeField] private float movementDuration;
+
+    [Header("Tween Scale")]
+    [SerializeField] private Vector3 scaleValue;
+    [SerializeField] private float scaleDuration;
+
+    [Header("Tween Alpha")]
+    [SerializeField] private float alphaDelay;
+    [SerializeField] private float alphaDuration;
 
     public void Initialize(string textValue, Vector3 position)
     {
         textMeshPro.text = textValue;
         transform.position = position;
+
+        transform.DOScale(scaleValue, scaleDuration).SetLoops(2, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        transform.DOMove(position + movementValue, movementDuration).SetEase(Ease.InOutSine);
+        textMeshPro.DOFaceFade(0, alphaDuration).SetDelay(alphaDelay).OnComplete(() => { Release(); });
+    }
+
+
+    void Update()
+    {
+
     }
 
     public void Fire()
@@ -34,4 +50,6 @@ public class HealthPop : MonoBehaviour, IPoolable
     {
        
     }
+
+
 }
